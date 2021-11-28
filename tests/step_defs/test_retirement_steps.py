bfa0052610eb29ba420a79848cec79228946e9b8
K-eli-wtech <kewhite3@my.waketech.edu>
2021-11-28 17:44:14 -0500

    Update to steps file

--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -1,5 +1,10 @@
 from pytest_bdd import scenario, parsers, given, when, then
-import retirement
+from retirement import *
+
+
+EXTRA_TYPES = {
+    'Number': int,
+}
 
 
 CONVERTERS = {
@@ -8,95 +13,33 @@ CONVERTERS = {
 }
 
 
+
 scenario('../features/retire_age.feature', 'Birth year entered in range')
 def test_retire_year():
     pass
 
 
-@given(parsers.cfparse('the input is numbers'))
-def check_input():
-    raise NotImplementedError(u'STEP: Given The input is numbers')
-
-
-@when(parsers.cfparse('birth "{year:Number}" is input', extra_types=EXTRA_TYPES))
-@when('birth "{year:Number}" is input')
-def year_in_check(check_input, year):
-    raise NotImplementedError(u'STEP: When Birth "<year>" is input')
-
-
-@then("The correct year and month is listed for age to retire along with how many years that will be")
-def step_impl():
-    raise NotImplementedError(
-        u'STEP: Then The correct year and month is listed for age to retire along with how many years that will be')
-
-
-@given(parsers.parse('the valid birth month is {birth_month}'),
-       target_fixture='check_all_inputs',
-       converters=CONVERTERS)
-@given(parsers.parse('the valid birth yar is {birth_year}'),
-       target_fixture='check_all_inputs',
-       converters=CONVERTERS)
-@given(parsers.parse('the retirement age is {retirement_age} and {remaining_months}'),
-       target_fixture='check_all_inputs',
-       converters=CONVERTERS)
-def check_all_inputs(birth_month, birth_year, retirement_age, remaining_months):
-    assert calculate_retirement_date(birth_year, birth_month, age_years=retirement_age, age_months=remaining_months)
-
-
-@when(parsers.parse('I calculate the retirement date'),
-      target_fixture='set_all_inputs',
-      converters=CONVERTERS)
-def set_all_inputs(birth_month, birth_year, retirement_age, remaining_months):
-    month, year = calculate_retirement_date(birth_year=birth_year,
-                                            birth_month=birth_month,
-                                            age_years=retirement_age,
-                                            age_months=remaining_months)
-    return month, year
-
-
-@then(parsers.parse('the month of retirement should be {month} as of 2021'),
-      target_fixture='calculate_retire_date',
-      converters=CONVERTERS)
-@then(parsers.parse('the year of retirement should be {year} as of November 18, 2021'),
-      target_fixture='calculate_retire_date',
-      converters=CONVERTERS)
-def calculate_retire_date(birth_year, birth_month, retirement_age, remaining_months):
-    month, year = set_all_inputs(birth_month, birth_year, retirement_age, remaining_months)
-    assert month == month
-    assert year == year
-
-
-@scenario('../features/retirement.feature', 'Calculate retirement age')
-def test_cra():
+@given('The input is numbers', target_fixture='input_valid')
+def input_valid():
     pass
 
 
-@given(parsers.parse('the {birth_year} entered is valid'),
-       target_fixture="determine_retirement_age",
-       converters=CONVERTERS)
-def determine_retirement_age(birth_year):
-    assert calculate_retirement_age(birth_year=birth_year)
+@when(parsers.cfparse('Birth "{year:Number}" is input', extra_types=EXTRA_TYPES))
+@when('Birth "<year>" is input')
+def check_year(year):
+    assert _validate_age_year(year)
+    year_correct = True
+    return year_correct
 
 
-@when(parsers.parse('I enter {birth_year}'),
-      target_fixture="set_birth_year",
-      converters=CONVERTERS)
-def set_birth_year(birth_year):
-    ra, rm = calculate_retirement_age(birth_year=birth_year)
-    return ra, rm
+@when(parsers.cfparse('Birth "{month:Number}" is input', extra_types=EXTRA_TYPES))
+@when('Birth "<month>" is input')
+def check_month(month):
+    assert _validate_birth_month(month)
+    month_correct = True
+    return month_correct
 
 
-@then(parsers.parse('the retirement age should be {retirement_age} years as of year 2021'),
-      target_fixture='execute_retirement_age',
-      converters=CONVERTERS)
-def execute_retirement_age(birth_year, retirement_age):
-    age, months = set_birth_year(birth_year=birth_year)
-    assert age == retirement_age
-
-
-@then(parsers.parse('the remaining months of the retirement age should be {remaining_months} months'),
-      target_fixture='execute_retirement_age',
-      converters=CONVERTERS)
-def execute_remaining_months(birth_year, remaining_months):
-    age, months = set_birth_year(birth_year=birth_year)
-    assert months == remaining_months
+@then('The correct year and month is listed for age to retire along with how many years that will be')
+def output_correct():
+    pass
